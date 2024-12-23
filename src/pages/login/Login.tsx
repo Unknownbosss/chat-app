5bffd3c2bc6762ee4ef01889935d28d685615d54
Unknownbosss <udeigweemmanuel0@gmail.com>
2024-12-23 23:03:19 +0100

    initialize state to manage login and sign up

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css/";
 import assets from "../../assets/assets";
 
 function Login() {
+  const [currentState, setCurrentState] = useState<string>("Sign Up");
+
   return (
     <div className="login">
       <img src={assets.logo_big} alt="" className="logo" />
       <form className="login-form">
-        <h2>Sign Up</h2>
-        <input
-          type="text"
-          placeholder="Username"
-          className="form-input"
-          required
-        />
+        <h2>{currentState}</h2>
+        {currentState === "Sign Up" ? (
+          <input
+            type="text"
+            placeholder="Username"
+            className="form-input"
+            required
+          />
+        ) : null}
         <input
           type="email"
           placeholder="Email Address"
@@ -26,15 +30,28 @@ function Login() {
           className="form-input"
           required
         />
-        <button type="submit"> Sign Up</button>
+        <button type="submit">
+          {currentState === "Sign Up" ? "Create Accoount" : "Login Now"}
+        </button>
         <div className="login-term">
           <input type="checkbox" />
           <p>Agree to the terms of use & privacy policy.</p>
         </div>
         <div className="login-forgot">
-          <p className="login-toggle">
-            Already have an account <span>click here</span>
-          </p>
+          {currentState === "Sign Up" ? (
+            <p className="login-toggle">
+              Already have an account{" "}
+              <span onClick={() => setCurrentState("Login")}> login Here</span>
+            </p>
+          ) : (
+            <p className="login-toggle">
+              Create an account
+              <span onClick={() => setCurrentState("Sign Up")}>
+                {" "}
+                click here
+              </span>
+            </p>
+          )}
         </div>
       </form>
     </div>
